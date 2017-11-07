b5727949bc8b2a086a20b7e22f1e5b53868a9775
a298582548 <105021010@live.asia.edu.tw>
2017-11-07 12:36:21 +0800

    0.0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        LoginFrame loginFrame = new LoginFrame();
-        loginFrame.setVisible(true);
+
+//        LoginFrame loginFrame = new LoginFrame();
+//        loginFrame.setVisible(true);
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true);
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,17 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame extends JFrame{
-    private JPanel jpn = new JPanel(new GridLayout(3,3,3,3));
+public class MainFrame extends JFrame {
+    private JPanel jpn = new JPanel(new GridLayout(3, 3, 4, 4));
     private Container cp;
     private JTextField jtf = new JTextField();
     private JButton jbtns[] = new JButton[9];
-    public MainFrame(){
+
+    public MainFrame() {
         initComp();
     }
-    private void initComp(){
-        this.setBounds(200,200,200,200);
-        cp.setLayout(new BorderLayout(5,5));
-        cp.add(jtf,BorderLayout.NORTH);
+
+    private void initComp() {
+        this.setBounds(200, 200, 500, 500);
+        cp = new Container();
+        this.setContentPane(cp);
+        cp.setLayout(new BorderLayout(5, 5));
+        cp.add(jtf, BorderLayout.NORTH);
+        cp.add(jpn, BorderLayout.CENTER);
+
+        for (int i = 0; i < 9; i++) {
+            jbtns[i] = new JButton(Integer.toString(i));
+            jpn.add(jbtns[i]);
+            jbtns[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton jButton = (JButton) e.getSource();
+
+                }
+            });
+
+
+        }
     }
 }
